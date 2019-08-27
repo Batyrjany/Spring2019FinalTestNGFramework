7096c39afd7bf84bea15ecf62f79292291946f63
Batyrjany <b.janyew@gmail.com>
2019-08-27 13:30:22 -0700

    githubtestforDEVbranch

---
+++ src/test/java/com/vytrack/tests/components/login_navigation/navigation.java
@@ -0,0 +1,12 @@
+package com.vytrack.tests.components.login_navigation;
+
+import org.testng.annotations.Test;
+
+public class navigation {
+
+    @Test
+    public void testingGitHubforDevBranch(){
+
+
+    }
+}
